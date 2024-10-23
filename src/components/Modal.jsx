6299208f95cd0fbe9544b6f18a9a6c87a3eb7087
Battugs Choijil <battugs@fibo.cloud>
2024-10-23 17:17:22 +0800

    [visible modala ]

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,10 @@
-const Modal = ({ children }) => {
+const Modal = ({ children, onClose }) => {
   return (
     <>
-      <div className="fixed top-0 left-0 w-full h-screen bg-black bg-opacity-60 z-10 " />
+      <div
+        className="fixed top-0 left-0 w-full h-screen bg-black bg-opacity-60 z-10 "
+        onClick={onClose}
+      />
       <dialog
         className="border-none rounded-md shadow-md p-0 overflow-hidden z-10"
         open={true}
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -4,22 +4,40 @@ import NewPost from "./NewPost";
 import Post from "./Post.";
 
 function PostList() {
+  const [modalVisible, setModalVisible] = useState(true);
   const [enteredBody, setEnteredBody] = useState([]);
   const [author, setAuthor] = useState([]);
 
+  function hideModalHandler() {
+    setModalVisible(false);
+  }
   function bodyChnageHandler(event) {
     setEnteredBody(event.target.value);
   }
 
-  return (
-    <>
-      <Modal>
+  let modalContent;
+  if (modalVisible) {
+    modalContent = (
+      <Modal onClose={hideModalHandler}>
         <NewPost
           onBodyChange={bodyChnageHandler}
           onAuthorChange={(event) => setAuthor(event.target.value)}
         />
       </Modal>
+    );
+  }
 
+  return (
+    <>
+      {modalVisible && (
+        <Modal onClose={hideModalHandler}>
+          <NewPost
+            onBodyChange={bodyChnageHandler}
+            onAuthorChange={(event) => setAuthor(event.target.value)}
+          />
+        </Modal>
+      )}
+      {/* {modalContent} */}
       <ul className="list-none max-w-3xl my-4 mx-auto py-4 grid grid-cols-3 gap-4 justify-center ">
         {/* <Post author="John Doe" body="Hello World" />
         <Post author="This is Battugs" body="Battugs is God" /> */}
